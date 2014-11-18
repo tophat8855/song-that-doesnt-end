64b2d3ec07a7e175c052865fd258c86a5d6c3279
tophat8855 <hrfarley@gmail.com>
2014-11-18 13:37:08 -0800

    works now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,15 @@
 class PagesController < ApplicationController
 
+  def verse2
+    render "verse-2"
+  end
+
+  def verse3
+    render 'verse-3'
+  end
+
+  def verse4
+    render 'verse-4'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#index"
+  get '/verse-2' => 'pages#verse2'
+  get '/verse-3' => 'pages#verse3'
+  get '/verse-4' => 'pages#verse4'
 end
